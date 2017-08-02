ea273df9bd6e64aef5ff32808f3abbb5a62fc05d
liangqy <1239551904@qq.com>
2017-08-02 15:16:40 +1200

    delete department

--- code/BidProgressManagementSystem/src/BidProgressManagementSystem.Web/Program.cs
+++ code/BidProgressManagementSystem/src/BidProgressManagementSystem.Web/Program.cs
@@ -14,8 +14,7 @@ namespace BidProgressManagementSystem
     {
         public static void Main(string[] args)
         {
-			InsertData();
-			PrintData();
+			//PrintData();
 
 			var host = new WebHostBuilder()
                 .UseKestrel()
@@ -27,45 +26,6 @@ namespace BidProgressManagementSystem
 
             host.Run();
 
-		}
-
-		private static void InsertData()
-		{
-			using (var context = new LibraryContext())
-			{
-				// Creates the database if not exists
-				context.Database.EnsureCreated();
-
-				// Adds a publisher
-				var publisher = new Publisher
-				{
-					Name = "Mariner Books"
-				};
-				context.Publisher.Add(publisher);
-
-				// Adds some books
-				context.Book.Add(new Book
-				{
-					ISBN = "978-0544003415",
-					Title = "The Lord of the Rings",
-					Author = "J.R.R. Tolkien",
-					Language = "English",
-					Pages = 1216,
-					Publisher = publisher
-				});
-				context.Book.Add(new Book
-				{
-					ISBN = "978-0547247762",
-					Title = "The Sealed Letter",
-					Author = "Emma Donoghue",
-					Language = "English",
-					Pages = 416,
-					Publisher = publisher
-				});
-
-				// Saves changes
-				context.SaveChanges();
-			}
 		}
 
 		private static void PrintData()
