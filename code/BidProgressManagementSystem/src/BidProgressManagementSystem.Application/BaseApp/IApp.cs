d1440d9bbb5c799b88cc41e077223185b21d61ba
liangqy <1239551904@qq.com>
2017-08-03 22:56:03 +0800

    adf

---
+++ code/BidProgressManagementSystem/src/BidProgressManagementSystem.Application/BaseApp/IApp.cs
@@ -0,0 +1,16 @@
+﻿using BidProgressManagementSystem.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BidProgressManagementSystem.Application.BaseApp
+{
+    public interface IApp
+    {
+
+    }
+    public interface IApp<TEntity, TPrimaryKey> : IApp where TEntity : BaseModel<TPrimaryKey>
+    {
+
+    }
+}
