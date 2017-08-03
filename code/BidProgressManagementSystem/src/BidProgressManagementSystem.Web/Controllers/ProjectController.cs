52350161a4054905f2834226d8a4c5b24986387e
liangqy <1239551904@qq.com>
2017-08-04 10:27:14 +1200

    project view

---
+++ code/BidProgressManagementSystem/src/BidProgressManagementSystem.Web/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BidProgressManagementSystem.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		public IActionResult GetAllPageList(int startPage, int pageSize)
+		{
+			return Json(new
+			{
+				rowCount = 100,
+				pageCount = 10,
+				rows =new int[10],
+			});
+		}
+	}
+}
